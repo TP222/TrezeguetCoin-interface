8352025d440bcfda2f72d087069a721d5afd9b6f
TP222 <tp222@pm.me>
2023-05-29 18:41:16 +0200

    added events and buffer and modifyed vite.config.ts

--- .pnp.cjs
+++ .pnp.cjs
@@ -36,7 +36,9 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@vitejs/plugin-react", "virtual:0128e31f7df78b38413c90dda42920080666deb7ba2c1cbf1178f4b2d52c8f1272be85a8aed9170189db1a690c107063d2e4bbd76a92330df68e82e8b97d632b#npm:4.0.0"],\
             ["@walletconnect/jsonrpc-types", "npm:1.0.3"],\
             ["@walletconnect/jsonrpc-utils", "npm:1.0.8"],\
+            ["buffer", "npm:6.0.3"],\
             ["ethers", "npm:6.4.0"],\
+            ["events", "npm:3.3.0"],\
             ["framer-motion", "virtual:0128e31f7df78b38413c90dda42920080666deb7ba2c1cbf1178f4b2d52c8f1272be85a8aed9170189db1a690c107063d2e4bbd76a92330df68e82e8b97d632b#npm:10.12.16"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:0128e31f7df78b38413c90dda42920080666deb7ba2c1cbf1178f4b2d52c8f1272be85a8aed9170189db1a690c107063d2e4bbd76a92330df68e82e8b97d632b#npm:18.2.0"],\
@@ -6005,7 +6007,9 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@vitejs/plugin-react", "virtual:0128e31f7df78b38413c90dda42920080666deb7ba2c1cbf1178f4b2d52c8f1272be85a8aed9170189db1a690c107063d2e4bbd76a92330df68e82e8b97d632b#npm:4.0.0"],\
             ["@walletconnect/jsonrpc-types", "npm:1.0.3"],\
             ["@walletconnect/jsonrpc-utils", "npm:1.0.8"],\
+            ["buffer", "npm:6.0.3"],\
             ["ethers", "npm:6.4.0"],\
+            ["events", "npm:3.3.0"],\
             ["framer-motion", "virtual:0128e31f7df78b38413c90dda42920080666deb7ba2c1cbf1178f4b2d52c8f1272be85a8aed9170189db1a690c107063d2e4bbd76a92330df68e82e8b97d632b#npm:10.12.16"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:0128e31f7df78b38413c90dda42920080666deb7ba2c1cbf1178f4b2d52c8f1272be85a8aed9170189db1a690c107063d2e4bbd76a92330df68e82e8b97d632b#npm:18.2.0"],\
